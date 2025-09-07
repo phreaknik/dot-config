306dd6752838f5994bea4f51c5aff8d3ea8fb0b2
John Boyd <john@coldnoise.net>
2025-09-07 02:33:00 -0500

    hypr: Switch to workspace 0 when eDP-1 is disabled

--- hypr/toggle-internal-display.sh
+++ hypr/toggle-internal-display.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env zsh
 
+# By default, leave internal display enabled
+should_disable=false
+
 # If external monitor is connected (HDMI-A-1) toggle the internal display
 # (eDP-1) according to the laptop lid state.
 if [[ "$(hyprctl monitors)" =~ "\sHDMI-A-[0-9]+" ]]; then
   if grep -q "closed" /proc/acpi/button/lid/LID0/state 2>/dev/null; then
-    hyprctl keyword monitor "eDP-1,disable"
-  else
-    hyprctl keyword monitor "eDP-1,preferred,preferred,auto"
+    should_disable=true
   fi
-# Otherwise, always make sure the internal display is enabled
-else
-  hyprctl keyword monitor "eDP-1,preferred,preferred,auto"
 fi
 
+if $should_disable; then
+  echo "DISABLE"
+  hyprctl keyword monitor "eDP-1,disable"
+  hyprctl dispatch workspace 0
+else
+  echo "ENABLE"
+  hyprctl keyword monitor "eDP-1,preferred,preferred,auto"
+fi
