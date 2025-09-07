534dc8b1ff2d579602ad509c0d7c3ffefd2fb7f2
John Boyd <john@coldnoise.net>
2025-09-07 02:33:09 -0500

    waybar: Complete overhaul to floating bubbles
    Inspired heavily by:
    https://github.com/d00m1k/SimpleBlueColorWaybar/tree/main

--- waybar/scripts/checkupdates.sh
+++ waybar/scripts/checkupdates.sh
@@ -11,12 +11,12 @@ num_updates=$((num_paru_updates + num_flatpak_updates))
 
 # Output for waybar
 if [ "$num_updates" -eq 0 ]; then
-    echo '{"text": "System up to date", "alt": "up-to-date", "class": "up-to-date", "tooltip": "System is up to date"}'
+    echo '{"text": "Up to date!", "alt": "up-to-date", "class": "up-to-date", "tooltip": "System is up to date"}'
 else
     if [ "$num_updates" -eq 1 ]; then
-        text="1 Update Pending"
+        text="1"
     else
-        text="$num_updates Updates Pending"
+        text="$num_updates updates"
     fi
     
     tooltip="Paru: $num_paru_updates updates\\nFlatpak: $num_flatpak_updates updates"
